4e2fc01b4398aaecf1713ca7d505eb73f305d17d
Nick Joyce <brawlyrox@gmail.com>
2018-11-16 08:06:11 +0000

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -41,7 +41,7 @@ UserSchema.methods.toJSON = function () {
     return _.pick(userObject, ['_id', 'email'])
 }
 
-
+// instance method
 UserSchema.methods.generateAuthToken = function () {
     const user = this
     const access = 'auth'
@@ -50,6 +50,24 @@ UserSchema.methods.generateAuthToken = function () {
     user.tokens.push({access, token})
     return user.save().then(() => {
         return token
+    })
+}
+
+// model method
+UserSchema.statics.findByToken = function (token) {
+    const User = this
+    let decoded
+
+    try {
+        decoded = jwt.verify(token, 'abc123')
+    } catch (e) {
+        return Promise.reject()
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     })
 }
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash')
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
+const { authenticate } = require('./middleware/authenticate')
 
 const app = express()
 const port = process.env.PORT
@@ -116,12 +117,18 @@ app.post('/users', (req, res) => {
     const user = new User(body)
     
     user.save().then(()=> {
+        // generate Auth Token to identify user and sign it to x-auth
         return user.generateAuthToken()
     }).then((token) => {
+        // send back the header and the user
         res.header('x-auth', token).send(user)
     }).catch ((e) => {
         res.status(400).send(e)
     })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
 })
 
 // message to let you know the server has started
